09ef72c91f28594c6982eda5782ff4d3db841bfe
Jade Guyenne <jade.guyenne@gmail.com>
2025-07-04 15:50:14 +0200

    Create RsaGenerationKey that generate a pair of public and private keys

---
+++ generate_rsa_key.py
@@ -0,0 +1,111 @@
+"""
+RSA key generation Utility
+
+This module allows you to:
+- Generate a new RSA key pair (2048 bits or more)
+- Export the public key as an unencrypted PEM file
+- Export the private key encrypted using PBKDF2 + HMAC-SHA512 + AES-256-GCM
+- Protect the private key with a passphrase
+
+Example usage :
+    text_encrypt_rsa_key = RsaKeyGeneration("encrypt_text", 2048)
+
+"""
+
+from Crypto.PublicKey import RSA
+from Crypto.PublicKey.RSA import RsaKey
+
+
+class RsaKeyGeneration:
+    """
+    Utility class to generate and securely export an RSA key pair.
+
+    Attributes:
+        nom (str) : Prefix used for naming the output key files
+        bits (int) : RSA key size in bits
+        passphrase (str) : Passphrase used to encrypt the private key
+
+    Private Methods:
+        _generate_rsa_key(bits): Generates an RSA key pair.
+        _save_public_key(rsa_key): Exports and saves the public key.
+        _save_private_key(rsa_key) : Exports and saves encrypted private key
+        _generate_and_save_rsa_key(bits) : Orchestrates generation and export
+
+    Output files:
+        - <name>_rsa_public_key.pem: Public key in PEM format (unencrypted)
+        - <name>rsa_private_key.pem: Private key in PEM format
+    """
+
+    def __init__(
+        self, nom: str, bits: int, passphrase: str = "-->very secret<--"
+    ) -> None:
+        """
+        Initializes the RsaKeyGeneration object and immediately generates and saves the key pair
+
+        Args:
+            nom (str): Name used as a prefix for output file names
+            bits (int): RSA key size in bits
+            passphrase (str): Passphrase used to protec the private key
+        """
+        self.bits = bits
+        self.nom = nom
+        self.passphrase = passphrase
+        self._generate_and_save_rsa_key(self.bits)
+
+    def _generate_rsa_key(self, bits: int) -> RsaKey:
+        """
+        Generates an RSA key pair of the specified size
+
+        Args:
+            bits (int): Length of the RSA key in bits
+
+        Returns:
+            RsaKey: The generated RSA key object
+        """
+        return RSA.generate(bits)
+
+    def _save_public_key(self, rsa_key: RsaKey) -> None:
+        """
+        Save public key in a pem file
+
+        Args:
+            rsa_key (RsaKey): The RSA key from wich to extract the public key
+        """
+        with open(f"{self.nom}_rsa_public_key.pem", "wb") as f:
+            public_rsa_key = rsa_key.public_key().export_key()
+            f.write(public_rsa_key)
+
+    def _save_private_key(self, rsa_key: RsaKey) -> None:
+        """
+        Save encrypted RSA private key into a pem file
+
+        Args:
+            rsa_key (RsaKey): The RSA key containing the private component
+        """
+
+        with open(f"{self.nom}rsa_private_key.pem", "wb") as f:
+
+            private_rsa_key = rsa_key.export_key(
+                passphrase=self.passphrase,
+                pkcs=8,
+                protection="PBKDF2WithHMAC-SHA512AndAES256-GCM",
+                prot_params={"iteration_count": 131072},
+            )
+
+            f.write(private_rsa_key)
+
+    def _generate_and_save_rsa_key(self, bits: int) -> None:
+        """
+        Generates an RSA key and saves both the public and private components
+
+        Args:
+            bits (int): Size of the key in bits
+        """
+        rsa_key = self._generate_rsa_key(bits)
+        self._save_public_key(rsa_key)
+        self._save_private_key(rsa_key)
+
+
+if __name__ == "__main__":
+    text_encrypt_rsa_key = RsaKeyGeneration("encrypt_text", 2048)
+    signature_encrypt_rsa_key = RsaKeyGeneration("encrypt_signature", 2048)
