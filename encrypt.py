910340223eff674de0b3845e625b6a1b4f3e6975
Jade Guyenne <jade.guyenne@gmail.com>
2025-07-04 16:12:59 +0200

    encrypt AES key and IV with RSA public key

--- encrypt.py
+++ encrypt.py
@@ -1,17 +1,18 @@
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.Hash import HMAC, SHA256
 from Crypto.Random import get_random_bytes
+from Crypto.PublicKey import RSA
 import json
 import base64
 
 ############################################
-#   AES Encryption
+#   AES 256-GCM Encryption
 ############################################
 
 path_file = "plaintext.txt"
 
 with open(path_file, "r") as f:
-    data = f.read()
+    data = f.read()  
 
 print(data)
 
@@ -24,6 +25,21 @@ iv = get_random_bytes(12)
 # Encrypt text
 cipher = AES.new(aes_key, AES.MODE_GCM, iv)
 ciphertext, signature = cipher.encrypt_and_digest(bytes_data)
+
+############################################
+#   Encryption of AES key and IV with RSA
+############################################
+
+# Retrieve RSA public key
+encrypt_key_public_rsa_key = RSA.import_key(open("encrypt_text_rsa_public_key.pem", "rb").read())
+
+# Encrypt AES and IV with rsa public key
+cipher_rsa = PKCS1_OAEP.new(encrypt_key_public_rsa_key)
+
+encrypted_aes_key = cipher_rsa.encrypt(aes_key)
+encrypted_iv = cipher_rsa.encrypt(iv)
+
+
 
 
 base64_ciphertext = base64.b64encode(ciphertext)
